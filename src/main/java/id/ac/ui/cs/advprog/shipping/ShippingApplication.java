ef22c7f383cd343c3fe2c364b7d42f101ace52d3
hilaldfzn <hilalfauzan9@gmail.com>
2024-04-06 21:09:56 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/shipping/ShippingApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.shipping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShippingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShippingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/shipping/ShippingApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.shipping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
