95dd54d418d4b48b528af77e94aca44278a4372f
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-04-17 18:45:14 +0700

    [DEPLOYMENT] add controller and HTML Templates

---
+++ src/main/java/id/ac/ui/cs/advprog/shipping/controller/ShipmentController.java
@@ -0,0 +1,21 @@
+package id.ac.ui.cs.advprog.shipping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/shipment")
+public class ShipmentController {
+    @GetMapping("/")
+    public String trackingPage() {
+        return "trackShippingPage";
+    }
+
+    @GetMapping("/result/{id}")
+    public String trackingResultPage(@PathVariable("id") String id){
+        return "resultPage";
+    }
+
+}
