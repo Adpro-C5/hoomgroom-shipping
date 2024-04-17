8922593dcd7ef91760f4bbab8aae420a75de5cf1
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-04-17 15:53:13 +0700

    [BUILD] update dependencies and properties for deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ group = "id.ac.ui.cs.advprog"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+	sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
@@ -49,41 +49,18 @@ dependencies {
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 }
 
-tasks.register<Test>("unitTest") {
-	description = "Runs unit tests."
-	group = "verification"
-
-	filter {
-		excludeTestsMatching("*FunctionalTest")
-	}
-}
-
-tasks.register<Test>("functionalTest") {
-	description = "Runs functional tests."
-	group = "verification"
-
-	filter {
-		includeTestsMatching("*FunctionalTest")
-	}
-}
-
-tasks.withType<Test>().configureEach {
-	useJUnitPlatform()
-}
-
 tasks.test {
-	filter {
-		excludeTestsMatching("*FunctionalTest")
-	}
-
-	finalizedBy(tasks.jacocoTestReport)
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
 }
-
 tasks.jacocoTestReport {
-	dependsOn(tasks.test)
-
+	classDirectories.setFrom(files(classDirectories.files.map {
+		fileTree(it) { exclude("**/*Application**") }
+	}))
+	dependsOn(tasks.test) // tests are required to run before generating the report
 	reports {
-		html.required = true
-		xml.required = true
+		xml.required.set(false)
+		csv.required.set(false)
+		html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
 	}
 }
