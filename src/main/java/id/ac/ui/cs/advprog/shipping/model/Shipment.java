9e11268f425fd621aedf13e5e3043ba59e0205a3
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-04-23 23:56:19 +0700

    [GREEN] Implement Shipment Model

--- src/main/java/id/ac/ui/cs/advprog/shipping/model/Shipment.java
+++ src/main/java/id/ac/ui/cs/advprog/shipping/model/Shipment.java
@@ -1,8 +1,11 @@
 package id.ac.ui.cs.advprog.shipping.model;
 
+import enums.ShippingStatus;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Builder
 @Getter
@@ -11,15 +14,31 @@ public class Shipment {
     String orderId;
     String status;
 
-    public Shipment(String id, String orderId) {
+    public Shipment() {
+        this.id = UUID.randomUUID().toString();
+        this.orderId = null;
+        this.status = ShippingStatus.MENUNGGU_VERIFIKASI.toString();
+    }
 
+    public Shipment(String id, String orderId) {
+        this.id = id;
+        this.orderId = orderId;
+        this.status = ShippingStatus.MENUNGGU_VERIFIKASI.toString();
     }
 
     public Shipment(String id, String orderId, String status) {
-
+        this.id = id;
+        this.orderId = orderId;
+        setStatus(status);
     }
 
-
+    public void setStatus(String status) {
+        if(ShippingStatus.contains(status)){
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException("Invalid status");
+        }
+    }
 }
 
 
