552099d89b4ebcbcd54f16034ad8195d59944716
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-04-24 11:04:33 +0700

    [RED] Change ShipmentRepository skeleton

--- src/main/java/id/ac/ui/cs/advprog/shipping/repository/ShipmentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/shipping/repository/ShipmentRepository.java
@@ -3,27 +3,25 @@ package id.ac.ui.cs.advprog.shipping.repository;
 import id.ac.ui.cs.advprog.shipping.model.Shipment;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class ShipmentRepository {
-    private List<Shipment> shipmentData;
+    private List<Shipment> shipmentData = new ArrayList<>();
     public Shipment findById(String id) {
         return null;
     }
 
-    public Shipment createShipment(String id, String orderId, String status) {
+    public Shipment findByOrderId(String orderId) {
         return null;
     }
 
-    public Shipment createShipment(String id, String orderId) {
+    public Shipment saveShipment(Shipment shipment) {
         return null;
     }
 
-    public Shipment editShipment(String id, String orderId, String status) {
+    public Shipment deleteShipment(String id) {
         return null;
-    }
-
-    public void deleteShipment(String id) {
     }
 }
