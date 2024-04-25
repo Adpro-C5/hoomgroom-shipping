50b4736bec3586e81e1d3691acd6f560c7cf9967
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-04-25 19:48:15 +0700

    [DB] Convert Shipment to Entity

--- src/main/java/id/ac/ui/cs/advprog/shipping/model/Shipment.java
+++ src/main/java/id/ac/ui/cs/advprog/shipping/model/Shipment.java
@@ -1,22 +1,35 @@
 package id.ac.ui.cs.advprog.shipping.model;
 
 import enums.ShippingStatus;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.util.UUID;
 
+
+@Entity
 @Builder
 @Getter
+@Table(name = "shipment")
 public class Shipment {
+    @Id
+    @Column(name = "id",nullable = false)
     String id;
+    @Column(name = "orderId",nullable = false)
     String orderId;
+    @Column(name = "status",nullable = false)
     String status;
 
     public Shipment() {
         this.id = UUID.randomUUID().toString();
         this.orderId = null;
+        this.status = ShippingStatus.MENUNGGU_VERIFIKASI.toString();
+    }
+
+    public Shipment(String orderId) {
+        this.id = UUID.randomUUID().toString();
+        this.orderId = orderId;
         this.status = ShippingStatus.MENUNGGU_VERIFIKASI.toString();
     }
 
