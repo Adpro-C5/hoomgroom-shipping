929c0673df11396e49a6937f3d9e20e826bb65dc
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-05-01 10:18:02 +0700

    [DB] Implement Repository with Database

--- src/main/java/id/ac/ui/cs/advprog/shipping/repository/ShipmentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/shipping/repository/ShipmentRepository.java
@@ -1,53 +1,48 @@
 package id.ac.ui.cs.advprog.shipping.repository;
 
 import id.ac.ui.cs.advprog.shipping.model.Shipment;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.util.ArrayList;
 import java.util.List;
-
 @Repository
 public class ShipmentRepository {
-    private List<Shipment> shipmentData = new ArrayList<>();
+    @Autowired
+    EntityManager entityManager;
+    @Transactional
     public Shipment findById(String id) {
-        for (Shipment shipment : shipmentData) {
-            if (shipment.getId().equals(id)) {
-                return shipment;
-            }
-        }
-        return null;
+        return entityManager.find(Shipment.class, id);
     }
-
+    @Transactional
     public Shipment findByOrderId(String orderId) {
-        for (Shipment shipment : shipmentData) {
-            if (shipment.getOrderId().equals(orderId)) {
-                return shipment;
-            }
-        }
-        return null;
+        return entityManager.createQuery("SELECT s FROM Shipment s WHERE s.orderId = :orderId", Shipment.class)
+                .setParameter("orderId", orderId)
+                .getSingleResult();
     }
-
+    @Transactional
     public Shipment saveShipment(Shipment shipment) {
-        int index = shipmentData.indexOf(shipment);
-        if (index == -1) {
-            shipmentData.add(shipment);
+        if (findById(shipment.getId()) == null) {
+            entityManager.persist(shipment);
             return shipment;
         } else {
-            shipmentData.set(index, shipment);
+            entityManager.merge(shipment);
             return null;
         }
     }
-
+    @Transactional
     public Shipment deleteShipment(String id) {
-        Shipment shipment = findById(id);
-        if (shipment != null) {
-            shipmentData.remove(shipment);
+        if (entityManager.find(Shipment.class, id) != null) {
+            Shipment shipment = entityManager.find(Shipment.class, id);
+            entityManager.remove(shipment);
             return shipment;
+        } else {
+            return null;
         }
-        return null;
     }
-
+    @Transactional
     public List<Shipment> findAll() {
-        return shipmentData;
+        return entityManager.createQuery("SELECT s FROM Shipment s", Shipment.class)
+                .getResultList();
     }
 }
