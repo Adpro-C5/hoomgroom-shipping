b9523c1e9ffd52eef82e384378e4d393c06d9638
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-05-06 16:46:58 +0700

    [COVERAGE] Fix RepositoryFactory Coverage

--- src/test/java/id/ac/ui/cs/advprog/shipping/factory/ShipmentRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/shipping/factory/ShipmentRepositoryTest.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.shipping.factory;
 
+import enums.ShippingStatus;
 import id.ac.ui.cs.advprog.shipping.repository.ShipmentRepository;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,19 @@ public class ShipmentRepositoryTest {
         ShipmentRepository shipmentRepository = shipmentRepositoryFactory.create();
         assertNotNull(shipmentRepository);
         assertTrue(shipmentRepository instanceof ShipmentRepository);
+    }
+
+    @Test
+    void testCreateWrongArgument(){
+        ShipmentRepositoryFactory shipmentRepositoryFactory = new ShipmentRepositoryFactory();
+        assertThrows(UnsupportedOperationException.class, () -> {
+            shipmentRepositoryFactory.create("1");
+        });
+        assertThrows(UnsupportedOperationException.class, () -> {
+            shipmentRepositoryFactory.create("1","88");
+        });
+        assertThrows(UnsupportedOperationException.class, () -> {
+            shipmentRepositoryFactory.create("1","88", ShippingStatus.DIKIRIM.getValue());
+        });
     }
 }
