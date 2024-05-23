46280bbfbe541ac504a9419fc590bcfd7cca6bbc
SamuelFarrel <samuel.farrel@ui.ac.id>
2024-05-23 21:47:56 +0700

    [CLEAN CODE] Fix EntityManager for Repository Creation

--- src/main/java/id/ac/ui/cs/advprog/shipping/factory/ShipmentRepositoryFactory.java
+++ src/main/java/id/ac/ui/cs/advprog/shipping/factory/ShipmentRepositoryFactory.java
@@ -1,11 +1,20 @@
 package id.ac.ui.cs.advprog.shipping.factory;
 
 import id.ac.ui.cs.advprog.shipping.repository.ShipmentRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class ShipmentRepositoryFactory implements Factory<ShipmentRepository>{
     @Override
     public ShipmentRepository create() {
-        return new ShipmentRepository();
+        ShipmentRepository shipmentRepository = null;
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("shipment");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManagerFactory.close();
+        shipmentRepository = new ShipmentRepository(entityManager);
+        entityManager.close();
+        return shipmentRepository;
     }
     @Override
     public ShipmentRepository create(String orderId) {
